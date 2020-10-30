c75760e6569c198ec6c7f0fea8be5cc15e7186a2
Neestackich <neestackich@gmail.com>
2020-10-30 17:42:33 +0300

    added news manager

--- TuTByNews/TuTByNews/Models/Managers/NewsManager.swift
+++ TuTByNews/TuTByNews/Models/Managers/NewsManager.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class NewsManager: NSObject {
+    
+    
+    // MARK: -Properties
+    
+    static let shared = NewsManager()
+    
+    // MARK: -Methods
+    
+    func getFeed(url: String, completion: ((Data) -> Void)?) {
+        guard let url = URL(string: url) else {
+            return
+        }
+        
+        let getXMLTask = URLSession.shared.dataTask(with: URLRequest(url: url)) { (feedInXml, response, error) in
+            if let error = error {
+                print(error)
+            } else {
+                guard let feedInXml = feedInXml else {
+                    return
+                }
+                
+                if let completion = completion {
+                    completion(feedInXml)
+                }
+            }
+        }
+        
+        getXMLTask.resume()
+    }
+}
