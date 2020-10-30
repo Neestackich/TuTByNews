eb4a8ff902ac1bf208c3937c7b525541e992f241
Neestackich <neestackich@gmail.com>
2020-10-30 17:40:33 +0300

    added entity

--- TuTByNews/TuTByNews/Models/Entities/RSSItem.swift
+++ TuTByNews/TuTByNews/Models/Entities/RSSItem.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct RSSItem {
+    var title: String
+    var description: String
+    var pubDate: String
+    var descriptionImages: [String]
+    var mediaImages: [String]
+}
