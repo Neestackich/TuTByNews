24707c2b5ff44e22f4eb45dfa1012ab37233b762
Neestackich <neestackich@gmail.com>
2020-10-31 22:00:02 +0300

    completed all methods

--- TuTByNews/TuTByNews/Models/Managers/LocationManager.swift
+++ TuTByNews/TuTByNews/Models/Managers/LocationManager.swift
@@ -5,4 +5,56 @@
 //  Created by Neestackich on 10/31/20.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = LocationManager()
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        
+        locationManager.delegate = self
+        locationManager.pausesLocationUpdatesAutomatically = true
+        locationManager.desiredAccuracy = .greatestFiniteMagnitude
+    }
+    
+    func getLocation() {
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        
+        if authorizationStatus == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            isValidLocation()
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+            
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        getLocation()
+    }
+    
+    func isValidLocation() {
+        if let location = locationManager.location {
+            let geocoder = CLGeocoder()
+            geocoder.reverseGeocodeLocation(location) { [] placeMark, error in
+                guard let placeMark = placeMark?.first else {
+                    return
+                }
+                
+                if let countryCode = placeMark.isoCountryCode {
+                    if countryCode == "RU" {
+                        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "true"), object: nil)
+                    } else {
+                        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "false"), object: nil)
+                    }
+                    
+                    print(countryCode)
+                }
+            }
+        }
+    }
+}
