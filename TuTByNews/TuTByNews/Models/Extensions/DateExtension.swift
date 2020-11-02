ae46fd0ac5bde378fe10a1f04b3a96c0e21bf781
Neestackich <neestackich@gmail.com>
2020-11-02 14:15:23 +0300

    added parse date method

--- TuTByNews/TuTByNews/Models/Extensions/DateExtension.swift
+++ TuTByNews/TuTByNews/Models/Extensions/DateExtension.swift
@@ -1,5 +1,5 @@
 //
-//  DateExtencion.swift
+//  DateExtension.swift
 //  TuTByNews
 //
 //  Created by Neestackich on 11/2/20.
@@ -8,5 +8,20 @@
 import UIKit
 
 extension Date {
-    
+    func getFormattedDate(dateToParse: String) -> String {
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "E, d MMM yyyy HH:mm:ss Z"
+        dateFormatterGet.locale = Locale(identifier: "en_GB")
+        
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "dd/MM/yyyy"
+        
+        let date = dateFormatterGet.date(from: dateToParse)
+        
+        if let date = date {
+            return dateFormatterPrint.string(from: date)
+        } else {
+            return dateToParse
+        }
+    }
 }
